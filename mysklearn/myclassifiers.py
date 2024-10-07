24d4fc1ccbab855ea232b4d6d6c6ddf106afbe47
Gina Sprint <sprint@gonzaga.edu>
2024-10-07 14:05:03 -0700

    initial

---
+++ mysklearn/myclassifiers.py
@@ -0,0 +1,168 @@
+from mysklearn import myutils
+from mysklearn.mysimplelinearregressor import MySimpleLinearRegressor
+
+class MySimpleLinearRegressionClassifier:
+    """Represents a simple linear regression classifier that discretizes
+        predictions from a simple linear regressor (see MySimpleLinearRegressor).
+
+    Attributes:
+        discretizer(function): a function that discretizes a numeric value into
+            a string label. The function's signature is func(obj) -> obj
+        regressor(MySimpleLinearRegressor): the underlying regression model that
+            fits a line to x and y data
+
+    Notes:
+        Terminology: instance = sample = row and attribute = feature = column
+    """
+
+    def __init__(self, discretizer, regressor=None):
+        """Initializer for MySimpleLinearClassifier.
+
+        Args:
+            discretizer(function): a function that discretizes a numeric value into
+                a string label. The function's signature is func(obj) -> obj
+            regressor(MySimpleLinearRegressor): the underlying regression model that
+                fits a line to x and y data (None if to be created in fit())
+        """
+        self.discretizer = discretizer
+        self.regressor = regressor
+
+    def fit(self, X_train, y_train):
+        """Fits a simple linear regression line to X_train and y_train.
+
+        Args:
+            X_train(list of list of numeric vals): The list of training instances (samples).
+                The shape of X_train is (n_train_samples, n_features)
+            y_train(list of obj): The target y values (parallel to X_train)
+                The shape of y_train is n_train_samples
+        """
+        pass # TODO: fix this
+
+    def predict(self, X_test):
+        """Makes predictions for test samples in X_test by applying discretizer
+            to the numeric predictions from regressor.
+
+        Args:
+            X_test(list of list of numeric vals): The list of testing samples
+                The shape of X_test is (n_test_samples, n_features)
+
+        Returns:
+            y_predicted(list of obj): The predicted target y values (parallel to X_test)
+        """
+        return [] # TODO: fix this
+
+class MyKNeighborsClassifier:
+    """Represents a simple k nearest neighbors classifier.
+
+    Attributes:
+        n_neighbors(int): number of k neighbors
+        X_train(list of list of numeric vals): The list of training instances (samples).
+                The shape of X_train is (n_train_samples, n_features)
+        y_train(list of obj): The target y values (parallel to X_train).
+            The shape of y_train is n_samples
+
+    Notes:
+        Loosely based on sklearn's KNeighborsClassifier:
+            https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
+        Terminology: instance = sample = row and attribute = feature = column
+        Assumes data has been properly normalized before use.
+    """
+    def __init__(self, n_neighbors=3):
+        """Initializer for MyKNeighborsClassifier.
+
+        Args:
+            n_neighbors(int): number of k neighbors
+        """
+        self.n_neighbors = n_neighbors
+        self.X_train = None
+        self.y_train = None
+
+    def fit(self, X_train, y_train):
+        """Fits a kNN classifier to X_train and y_train.
+
+        Args:
+            X_train(list of list of numeric vals): The list of training instances (samples).
+                The shape of X_train is (n_train_samples, n_features)
+            y_train(list of obj): The target y values (parallel to X_train)
+                The shape of y_train is n_train_samples
+
+        Notes:
+            Since kNN is a lazy learning algorithm, this method just stores X_train and y_train
+        """
+        self.X_train = X_train
+        self.y_train = y_train
+
+    def kneighbors(self, X_test):
+        """Determines the k closes neighbors of each test instance.
+
+        Args:
+            X_test(list of list of numeric vals): The list of testing samples
+                The shape of X_test is (n_test_samples, n_features)
+
+        Returns:
+            distances(list of list of float): 2D list of k nearest neighbor distances
+                for each instance in X_test
+            neighbor_indices(list of list of int): 2D list of k nearest neighbor
+                indices in X_train (parallel to distances)
+        """
+        return [], [] # TODO: fix this
+
+    def predict(self, X_test):
+        """Makes predictions for test instances in X_test.
+
+        Args:
+            X_test(list of list of numeric vals): The list of testing samples
+                The shape of X_test is (n_test_samples, n_features)
+
+        Returns:
+            y_predicted(list of obj): The predicted target y values (parallel to X_test)
+        """
+        return [] # TODO: fix this
+
+class MyDummyClassifier:
+    """Represents a "dummy" classifier using the "most_frequent" strategy.
+        The most_frequent strategy is a Zero-R classifier, meaning it ignores
+        X_train and produces zero "rules" from it. Instead, it only uses
+        y_train to see what the most frequent class label is. That is
+        always the dummy classifier's prediction, regardless of X_test.
+
+    Attributes:
+        most_common_label(obj): whatever the most frequent class label in the
+            y_train passed into fit()
+
+    Notes:
+        Loosely based on sklearn's DummyClassifier:
+            https://scikit-learn.org/stable/modules/generated/sklearn.dummy.DummyClassifier.html
+    """
+    def __init__(self):
+        """Initializer for DummyClassifier.
+
+        """
+        self.most_common_label = None
+
+    def fit(self, X_train, y_train):
+        """Fits a dummy classifier to X_train and y_train.
+
+        Args:
+            X_train(list of list of numeric vals): The list of training instances (samples).
+                The shape of X_train is (n_train_samples, n_features)
+            y_train(list of obj): The target y values (parallel to X_train)
+                The shape of y_train is n_train_samples
+
+        Notes:
+            Since Zero-R only predicts the most frequent class label, this method
+                only saves the most frequent class label.
+        """
+        pass # TODO: fix this
+
+    def predict(self, X_test):
+        """Makes predictions for test instances in X_test.
+
+        Args:
+            X_test(list of list of numeric vals): The list of testing samples
+                The shape of X_test is (n_test_samples, n_features)
+
+        Returns:
+            y_predicted(list of obj): The predicted target y values (parallel to X_test)
+        """
+        return [] # TODO: fix this
---
+++ test_myclassifiers.py
@@ -0,0 +1,27 @@
+# pylint: skip-file
+import numpy as np
+from scipy import stats
+
+from mysklearn.mysimplelinearregressor import MySimpleLinearRegressor
+from mysklearn.myclassifiers import MySimpleLinearRegressionClassifier,\
+    MyKNeighborsClassifier,\
+    MyDummyClassifier
+
+# note: order is actual/received student value, expected/solution
+def test_simple_linear_regression_classifier_fit():
+    assert False is True # TODO: fix this
+
+def test_simple_linear_regression_classifier_predict():
+    assert False is True # TODO: fix this
+
+def test_kneighbors_classifier_kneighbors():
+    assert False is True # TODO: fix this
+
+def test_kneighbors_classifier_predict():
+    assert False is True # TODO: fix this
+
+def test_dummy_classifier_fit():
+    assert False is True # TODO: fix this
+
+def test_dummy_classifier_predict():
+    assert False is True # TODO: fix this
